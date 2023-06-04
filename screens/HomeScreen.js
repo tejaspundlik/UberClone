65140409d2f1803b5f16b5fbba2d79cd6871e0ad
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-06-04 22:18:33 +0530

    Fix undefined error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,11 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+	const dispatch = useDispatch();
 	return (
 		<SafeAreaView style={tw`bg-white h-full`}>
 			<View style={tw`p-5`}>
@@ -17,6 +20,46 @@ const HomeScreen = () => {
 					}}
 					source={require("../images/logo.png")}
 				/>
+				<GooglePlacesAutocomplete
+					nearbyPlacesAPI="GooglePlacesSearch"
+					placeholder="Where From?"
+					debounce={400}
+					styles={{
+						container: {
+							flex: 0,
+							marginBottom: 0,
+						},
+						textInput: {
+							fontSize: 18,
+						},
+					}}
+					query={{
+						key: GOOGLE_MAPS_APIKEY,
+						language: "en",
+					}}
+					onPressIn={(data, details = null) => {
+						dispatch(
+							setOrigin({
+								location: details.geometry.location,
+								description: data.description,
+							})
+						);
+						dispatch(setDestination(null));
+					}}
+					onPressOut={(data, details = null) => {
+						dispatch(
+							setOrigin({
+								location: details.geometry.location,
+								description: data.description,
+							})
+						);
+						dispatch(setDestination(null));
+					}}
+					fetchDetails={true}
+					returnKeyType={"search"}
+					enablePoweredByContainer={false}
+					minLength={2}
+				/>
 			</View>
 			<NavOptions />
 		</SafeAreaView>
@@ -24,5 +67,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({});
