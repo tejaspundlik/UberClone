1b41f16a527962f7b8103107c20a3c5da8cb56f1
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-06-04 20:55:22 +0530

    Fix bundling error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const HomeScreen = () => {
 	return (
