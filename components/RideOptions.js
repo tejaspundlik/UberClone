71ab252fb8dc2dd56c8fd566728d0c133e72ad49
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-06-05 20:19:03 +0530

    Start working on rideoptions

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const RideOptions = () => {
 	return (
-		<View>
-			<Text>RideOptions</Text>
-		</View>
+		<SafeAreaView style={tw`bg-white flex-grow`}>
+			<Text style={tw`text-center text-xl `}>Select A Ride</Text>
+		</SafeAreaView>
 	);
 };
 
