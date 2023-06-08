c7b410dee50345f1408ff92370030f39309acf99
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-06-08 22:38:31 +0530

    Demo remaining

--- components/Confirm.js
+++ components/Confirm.js
@@ -5,7 +5,7 @@ import tw from "tailwind-react-native-classnames";
 const Confirm = () => {
 	return (
 		<View style={tw`flex-grow bg-black`}>
-			<Text style={tw`text-white text-4xl text-center`}>
+			<Text style={tw`text-white text-4xl text-center mt-36`}>
 				Booking Has Been Confirmed
 			</Text>
 		</View>
