10b33bb29ca1ca14d0a469e3edb77d2718298a04
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2023-06-08 21:19:38 +0530

    The navSlice has to have setTravel not just travel

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -28,6 +28,6 @@ export const { setOrigin, setDestination, setTravelTimeInformation } =
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
 export const selectTravelTimeInformation = (state) =>
-	state.nav.travelTimeInformation;
+	state.nav.setTravelTimeInformation;
 
 export default navSlice.reducer;
